cf606c7470f6d42cb98764edbacab9b43f10a3da
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-04-08 16:09:14 -0300

    chore: consuming api data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,14 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  photos = [ ];
+  photos: Object[] = [];
   
   constructor(http: HttpClient) {
-    console.log(http);
-
+   
+    const observable = http.get<Object[]>('http://localhost:3000/flavio/pgfhotos')
+    .subscribe(
+      photos => this.photos = photos,
+      err => console.log(err.message)
+      );
   }
 }
