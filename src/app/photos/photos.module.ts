db1a3c3a21186e25cd1002e81c4ad63dcb03be80
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-04-11 18:33:06 -0300

    chore: routing in the applicatio using modules

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
     declarations: [ 
         PhotoComponent, 
-        PhotoListComponent 
+        PhotoListComponent, PhotoFormComponent 
     ],
     imports: [ 
         HttpClientModule, 
