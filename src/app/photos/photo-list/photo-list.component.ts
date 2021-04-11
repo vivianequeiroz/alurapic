1c46d67d79e1ad08ea0fbfa08fa04f3a6cbf501c
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-04-11 18:50:37 -0300

    chore: parameterize a route and access the value with activated route

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotoService } from '../photo/photo.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-photo-list',
@@ -11,14 +12,16 @@ export class PhotoListComponent implements OnInit {
   photos: any[] = [];
   
   //only for dependency injection
-  constructor(private photoService: PhotoService) {
-   
-  }
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
-
+  
+    const userName = this.activatedRoute.snapshot.params.userName;
     this.photoService
-      .listFromUser('flavio')
+      .listFromUser(userName)
       .subscribe(photos => this.photos = photos);
   }
 }
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -13,6 +13,6 @@ export class PhotoService {
         
         listFromUser(userName: string) {
           return this.http
-            .get<Photo[]>(API + '/flavio/photos');
+            .get<Photo[]>(API + userName + '/photos');
         }
 }
