742580ee370b14a96d8081d24b87e6e20735898d
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-04-11 18:43:35 -0300

    chore: dealing with non existing routes

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
 
     { path: 'user/flavio', component: PhotoListComponent },
-    { path: 'p/add', component: PhotoFormComponent }
+    { path: 'p/add', component: PhotoFormComponent },
+    { path: '**', component: NotFoundComponent }
 ]
-
 @NgModule({
 
     imports: [ RouterModule.forRoot(routes) ],
