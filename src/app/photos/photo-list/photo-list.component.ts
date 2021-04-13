6f429731d21e75cdfc3fa987552420f92a3f8439
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-04-13 19:26:58 -0300

    chore: subject from rxjs and ondestroy to interrupt a non explicit declaration of completion

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from 'src/app/photo/photo';
 
@@ -8,16 +10,24 @@ import { Photo } from 'src/app/photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
   // properties
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
   
   //only for dependency injection
   constructor(private activatedRoute: ActivatedRoute) { }
-
+  
   ngOnInit(): void {
     
     this.photos = this.activatedRoute.snapshot.data.photos;
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 }
