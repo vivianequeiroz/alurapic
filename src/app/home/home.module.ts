1191eba1dacdd587fd8845f79bc28a9e75deb979
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-04-20 16:49:15 -0300

    chore: form validation

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { SignInComponent } from './signin/signin.component';
+import { ReactiveFormsModule  } from '@angular/forms';
 
+import { SignInComponent } from './signin/signin.component';
 @NgModule ({
-    declarations: [ SignInComponent ]
+    declarations: [ SignInComponent ],
+    imports: [ 
+        CommonModule,
+        ReactiveFormsModule,
+    ]
 })
 export class HomeModule { }
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,9 +1,20 @@
-
-
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './signin.component.html'
 })
 
-export class SignInComponent { }
+export class SignInComponent implements OnInit { 
+
+    loginForm: FormGroup;
+
+    constructor( private formBuilder: FormBuilder) { }
+
+    ngOnInit(): void {
+        this.loginForm = this.formBuilder.group({
+            userName: ['', Validators.required], 
+            password: ['', Validators.required]
+        });
+    }
+ }
