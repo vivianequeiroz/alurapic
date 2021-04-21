499a3b01b7c7627c46e5268897539e4b77eefb5c
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-04-21 19:35:11 -0300

    chore: viewchild to manipulate the template and add focus on input

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/auth.service';
@@ -10,6 +10,7 @@ import { AuthService } from 'src/app/core/auth.service';
 export class SignInComponent implements OnInit { 
 
     loginForm: FormGroup;
+    @ViewChild('userNameInput')userNameInput: ElementRef<HTMLInputElement>;
 
     constructor( 
         private formBuilder: FormBuilder,
@@ -36,6 +37,7 @@ export class SignInComponent implements OnInit {
                 err => { 
                     console.log(err);
                     this.loginForm.reset();
+                    this.userNameInput.nativeElement.focus();
                     alert('Invalid user or password');
                 }
             );
