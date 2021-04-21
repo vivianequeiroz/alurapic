50daa23f2ef4b77c7d03d82bb11818d8dd945a94
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-04-21 19:20:35 -0300

    chore: sending credentials to API

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -9,7 +10,10 @@ export class SignInComponent implements OnInit {
 
     loginForm: FormGroup;
 
-    constructor( private formBuilder: FormBuilder) { }
+    constructor( 
+        private formBuilder: FormBuilder,
+        private authService: AuthService
+    ) { }
 
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
@@ -17,4 +21,25 @@ export class SignInComponent implements OnInit {
             password: ['', Validators.required]
         });
     }
+
+    login() {
+
+        const userName = this.loginForm.get('userName').value;  
+        const password = this.loginForm.get('password').value;  
+
+        this.authService
+            .authenticate(userName, password)
+            .subscribe(
+                () => console.log('Autenticado.'),
+                err => { 
+                    console.log(err);
+                    this.loginForm.reset();
+                    alert('Invalid user or password');
+                }
+            );
+        }   
  }
+
+function subscribe(arg0: () => void, arg1: (err: any) => void) {
+    throw new Error('Function not implemented.');
+}
