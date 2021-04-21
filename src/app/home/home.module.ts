663e0d82e515248868628e5b10aaf4fec0a87c8c
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-04-21 18:53:00 -0300

    chore: componentization of login messages

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule  } from '@angular/forms';
 
 import { SignInComponent } from './signin/signin.component';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 @NgModule ({
     declarations: [ SignInComponent ],
     imports: [ 
         CommonModule,
         ReactiveFormsModule,
+        VMessageModule
     ]
 })
 export class HomeModule { }
