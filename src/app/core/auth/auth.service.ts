2b7be8860eeecbb1611b159a9a89f95e9b652f30
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-04-22 11:36:17 -0300

    chore: checking if user is logged through header response

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000'
 @Injectable({
@@ -11,7 +12,16 @@ export class AuthService {
 
     authenticate(userName: string, password: string) {
 
-      return this.http.post(API_URL + '/user/login', {userName, password})
+      return this.http
+        .post(
+          API_URL + '/user/login', 
+          { userName, password }, 
+          { observe: 'response'} 
+        )
+        .pipe(tap(res => {
+          const authToken = res.headers.get('x-acess-token');
+          console.log(`User ${userName} authenticated with token ${authToken}`);
+        }))
     }
    
 }
