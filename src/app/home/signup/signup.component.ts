017436861b13f59d39be0b6e3a47cbb41ad25eb5
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-04-29 11:15:15 -0300

    chore: children routes and outlet route

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
+import { PlatformDetectorService } from 'src/app/core/plataform-detector/platform-detector.service';
 import { toLowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 import { NewUser } from './new-user';
 import { SignUpService } from './signup.service';
@@ -12,12 +14,14 @@ import { UserNotTakenValidatorService } from './user-not-taken.validator.service
 export class SignUpComponent implements OnInit { 
 
     signupForm: FormGroup;
+    @ViewChild('emailInput') emailInput: ElementRef<HTMLInputElement>;
 
     constructor(
         private formBuilder: FormBuilder,
         private userNotTakenValidatorService: UserNotTakenValidatorService,
         private signupService: SignUpService,
-        private router: Router
+        private router: Router,
+        private platformDetectorService: PlatformDetectorService
     ) {}
 
 
@@ -54,6 +58,9 @@ export class SignUpComponent implements OnInit {
                 ]
             ]
         });
+
+        this.platformDetectorService.isPlatformBrowser() && 
+        this.emailInput.nativeElement.focus();
     }
 
     signup() { 
