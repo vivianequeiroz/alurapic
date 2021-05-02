2764166d3a0021e4c193b8e41f5ab5fd509f2507
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-02 18:38:30 -0300

    chore: adding initial form to upload images

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PhotoFormComponent } from './photo-form.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 
 @NgModule({
-    declarations: [PhotoFormComponent],
-    imports: [ CommonModule ]
+    declarations: [
+        PhotoFormComponent
+    ],
+    imports: [ 
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        FormsModule
+    ]
 })
 export class PhotoFormModule { }
