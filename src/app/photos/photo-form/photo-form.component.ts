958139e11198fc5f9f6760e9223294ec2cf711ee
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-02 19:05:20 -0300

    chore: obtaining form data to submit to backend

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -9,6 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class PhotoFormComponent implements OnInit {
 
   photoForm: FormGroup;
+  file: File;
   
   constructor(
     private formBuilder: FormBuilder
@@ -21,5 +22,13 @@ export class PhotoFormComponent implements OnInit {
       description: ['', Validators.maxLength(300)],
       allowComments: [true]
     })
+  }
+
+  upload() {
+    const description = this.photoForm.get('description').value;
+    const allowComments = this.photoForm.get('allowComments').value;
+    console.log(description);
+    console.log(allowComments);
+    console.log(this.file);
   }
 }
