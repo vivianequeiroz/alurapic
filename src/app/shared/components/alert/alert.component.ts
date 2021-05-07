530d7e38f9e38fd9320ea70e051718e8a5dfed86
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-07 18:52:08 -0300

    chore: componente template creation

--- src/app/shared/components/alert/alert.component.ts
+++ src/app/shared/components/alert/alert.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Alert } from './alert';
+import { Alert, AlertType } from './alert';
 import { AlertService } from './alert.service'
 
 @Component({
@@ -29,6 +29,23 @@ export class AlertComponent {
     }
 
     removeAlert(alertToRemove: Alert) {
+
         this.alerts = this.alerts.filter(alert => alert != alertToRemove)
+    }
+
+    getAlertClass(alert: Alert) {
+        if(!alert) return '';
+        
+        switch (alert.alertType) {
+            
+            case AlertType.DANGER:
+                return 'alert alert-danger'
+            case AlertType.INFO:
+                return 'alert alert-info'
+            case AlertType.SUCCESS:
+                return 'alert alert-success'
+            case AlertType.WARNING:
+                return 'alert alert-warning'
+        }
     }
 }
