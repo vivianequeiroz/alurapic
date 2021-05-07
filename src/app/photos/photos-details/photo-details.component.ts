bbfe6971c38deb70dac101ea59d0b1a2d43fe726
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-07 19:34:33 -0300

    chore: adding global messages through components and service created

--- src/app/photos/photos-details/photo-details.component.ts
+++ src/app/photos/photos-details/photo-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from 'src/app/core/user/user.service';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
 
 import { Photo } from '../photo/photo';
@@ -18,7 +19,8 @@ export class PhotoDetailsComponent implements OnInit{
         private route: ActivatedRoute,
         private photoService: PhotoService,
         private router: Router,
-        private alertService: AlertService
+        private alertService: AlertService,
+        private userService: UserService
     ) { }
     
     ngOnInit(): void {
@@ -32,11 +34,12 @@ export class PhotoDetailsComponent implements OnInit{
         .removePhoto(this.photoId)
         .subscribe(
             () => {
-            this.alertService.success('Photo removed')
-            this.router.navigate([''])
+            this.alertService.success('Photo removed', true)
+            this.router.navigate(['/user', this.userService.getUserName()])
             },
             err => {
-                console.log(err);this.alertService.warning('Photo could not be deleted');
+                console.log(err);
+                this.alertService.warning('Photo could not be deleted', true);
             });
     }
 }
--- src/app/shared/components/alert/alert.service.ts
+++ src/app/shared/components/alert/alert.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { Alert, AlertType } from './alert';
 
@@ -6,31 +7,49 @@ import { Alert, AlertType } from './alert';
 export class AlertService {
 
     alertSubject: Subject<Alert> = new Subject<Alert>();
+    keepAfterRouteChange = false;
 
-    success(message: string) {
-        this.alert(AlertType.SUCCESS, message);
+    constructor(router: Router) {
+
+        router.events.subscribe(event => {
+            if(event instanceof NavigationStart) {
+                if(this.keepAfterRouteChange) {
+                    this.keepAfterRouteChange = false
+                } else {
+                    this.clear();
+                }
+            }
+        })
     }
 
-    warning(message: string) {
-        this.alert(AlertType.WARNING, message);
+    success(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.SUCCESS, message, keepAfterRouteChange);
     }
 
-    danger(message: string) {
-        this.alert(AlertType.DANGER, message);
+    warning(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.WARNING, message, keepAfterRouteChange);
+    }
+
+    danger(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.DANGER, message, keepAfterRouteChange);
     }
 
 
-    info(message: string) {
-        this.alert(AlertType.INFO, message);
+    info(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.INFO, message, keepAfterRouteChange);
     }
 
 
-    private alert(alertType: AlertType, message: string) {
-
+    private alert(alertType: AlertType, message: string, keepAfterRouteChange: boolean) {
+        this.keepAfterRouteChange = keepAfterRouteChange;
         this.alertSubject.next(new Alert(alertType, message));
     }
 
     getAlert() {
         return this.alertSubject.asObservable();
- }
+    }
+
+    clear() {
+        this.alertSubject.next(null);
+    }
 }
