53e88e15d03d1fd93452cf7011eab86ff0837e79
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-07 15:26:41 -0300

    refact: automatic display of comments after publish

--- src/app/photos/photos-details/photos-comments/photo-comments.component.ts
+++ src/app/photos/photos-details/photos-comments/photo-comments.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+
 import { PhotoComment } from '../../photo/photo-comment';
 import { PhotoService } from '../../photo/photo.service';
 
@@ -29,14 +31,14 @@ export class PhotoCommentsComponent implements OnInit {
     }
 
     save() {
-        console.log('Salvando')
+        
         const comment = this.commentForm.get('comment').value as string;
-        this.photoService
-            .addComment(this.photoId, comment)
-            .subscribe(() => {
-                this.commentForm.reset();
-                alert('Comentário adicionado com sucesso');
-            });
-        console.log(this.commentForm);
+        this.comments$ = this.photoService
+                .addComment(this.photoId, comment)
+                .pipe(switchMap(() => this.photoService.getComments(this.photoId)))
+                .pipe(tap(() => {
+                    this.commentForm.reset();
+                    alert('Comentário adicionado com sucesso');
+          }));
     }
 }
