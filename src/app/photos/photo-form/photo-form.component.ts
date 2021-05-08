502f3133b82ef0632ff8c3a1269bb8a34c46998b
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-08 16:01:45 -0300

    chore: displaying upload progress to user

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,6 +1,9 @@
+import { HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
+
 import { UserService } from 'src/app/core/user/user.service';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { PhotoService } from '../photo/photo.service';
@@ -15,6 +18,7 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File;
   preview: string;
+  percentDone = 0;
   
   constructor(
     private formBuilder: FormBuilder,
@@ -38,11 +42,22 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.get('allowComments').value;
     
     this.photoService
-      .upload(description, allowComments, this.file)
-      .subscribe(() => {
-        this.alertService.success('Upload completed.', true)
-        this.router.navigate(['/user', this.userService.getUserName()]);
-      })
+        .upload(description, allowComments, this.file)
+        .pipe(finalize(()=> {
+          this.router.navigate(['/user', this.userService.getUserName()]);
+        }))
+        .subscribe((event: HttpEvent<any>) => {
+          if(event.type == HttpEventType.UploadProgress) {
+            this.percentDone = Math.round(100 * event.loaded / event.total);
+          } else if(event instanceof  HttpResponse) {
+            this.alertService.success('Upload complete', true);
+          }
+        },
+        err => {
+          console.log(err);
+          this.alertService.danger('Upload error!', true);
+        }
+      );
   }
 
   handleFile(file: File) {
