69549ddbfaee72c876269a461ca9c010d530d934
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-08 16:50:39 -0300

    chore: using an interceptor to globally apply the loading bar

--- src/app/shared/loading/loading.module.ts
+++ src/app/shared/loading/loading.module.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { LoadingComponent } from './loading.component';
+import { LoadingInterceptor } from './loading.interceptor';
 
 @NgModule({
      declarations: [
@@ -12,6 +14,11 @@ import { LoadingComponent } from './loading.component';
      ],
      imports: [
          CommonModule
-     ]
+     ],
+     providers: [{
+         provide: HTTP_INTERCEPTORS,
+         useClass: LoadingInterceptor,
+         multi: true
+     }]
 })
 export class LoadingModule { }
