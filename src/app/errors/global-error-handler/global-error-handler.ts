47b31ce1aa7cb2c5b29f4c5d1cb21bb34f2da120
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-09 21:10:04 -0300

    chore: exhibiting messages at the client side

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,11 +1,22 @@
 import { ErrorHandler } from '@angular/core';
-import * as StacktTrace from 'stacktrace-js';
+import * as StackTrace from 'stacktrace-js';
 
 
 export class GlobalErrorHandler implements ErrorHandler {
 
     handleError(error: any): void {
         console.log('Passou por error handler');
-        throw error;
+
+        const message = error.message ? error.message : error.toString();
+        StackTrace
+            .fromError(error)
+            .then(stackFrames => {
+                const stackAsString = stackFrames
+                    .map(sf => sf.toString())
+                    .join('\n')
+
+                    console.log(message);
+                    console.log(stackAsString);
+            });
     }
 }
